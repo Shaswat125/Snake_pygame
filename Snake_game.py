e25ba53e1d72e2190c0fe0730023affee8efe102
SHASWAT <shaswatanand125@gmail.com>
2021-05-29 23:40:17 +0530

    Snake's Head init (Square)

--- Snake_game.py
+++ Snake_game.py
@@ -1,41 +1,55 @@
 import pygame
-x=pygame.init()
-
-##It displays the type of x
-##print(x)
-##We need to make a window for game playing :
-gameWindow=pygame.display.set_mode((1200,500))
-
-##this takes tuple as height and width of window gameWin is var
-
-##to set the title of game window
-pygame.display.set_caption("Roy's game")
-
-##to end the game we need a variable, if this var is true then game ends
+pygame.init()
+width=900
+height=600
+##Game window
+gameWindow=pygame.display.set_mode((width, height))
+pygame.display.set_caption("MIck's Snake game")
+pygame.display.update()
+##Game specific varibles
 exit_game=False
-game_over=False##to ask wheter to restart
+game_over=False
+x=60##initial position of snake
+y=60
+sn_size=30
 
-i=0
-##we create game loop that handles all operations within a game
-while(not exit_game):
-    ##this returns what happens in a window this shows which key presses or handled
-    for event in pygame.event.get():
-        print(event)##this tells which event is done
-    ##we just need to handle these events for game
-        ##go to pygame.events docs for more events
 
-        
+##Colors
+white=(255,255,255)
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+black=(0,0,0)
+
+
+##Game Loop
+while not exit_game:
+    for event in pygame.event.get(): 
+        ##print(event)
         if event.type==pygame.QUIT:
             exit_game=True
-##this ends the game when u click on cross of window
 
-##For handling keypress in keyboard
         if event.type==pygame.KEYDOWN:
-            if(event.key==pygame.K_RIGHT):
-                print("right key pressed")
-
+            if event.key==pygame.K_RIGHT:
+                x=x+10
+       
+            if event.key==pygame.K_LEFT:
+                x=x-10
+       
+            if event.key==pygame.K_UP:
+                y=y-10
         
-##when game loop ends the pygame should end and python should end
+            if event.key==pygame.K_DOWN:
+                y=y+10
+
+    #window color
+    gameWindow.fill(white)
+
+    ##Making head of snake
+    pygame.draw.rect(gameWindow, red, [x,y,sn_size,sn_size])
+
+        #after every change update
+    pygame.display.update()
+    
 pygame.quit()
 quit()
-
