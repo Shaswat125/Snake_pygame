076d60ddeced52d664cd7cb10e2ff39f060b3bdb
SHASWAT <shaswatanand125@gmail.com>
2021-06-01 20:30:20 +0530

    increasing length of snake upon eating food

--- Snake_game.py
+++ Snake_game.py
@@ -1,94 +1,121 @@
 import pygame
 import random
+
 pygame.init()
-width=900
-height=600
+width = 900
+height = 600
 ##Game window
-gameWindow=pygame.display.set_mode((width, height))
-pygame.display.set_caption("MIck's Snake game")
+gameWindow = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Mick's Snake game")
 pygame.display.update()
 ##Game specific varibles
-exit_game=False
-game_over=False
-x=60##initial position of snake
-y=60
-sn_size=20
-clock=pygame.time.Clock()#it handles the clock of game
-fps=60
-in_velocity=5
-speed_x=5
-speed_y=0##we give speed or velocity to our snake
+exit_game = False
+game_over = False
+x = 60  ##initial position of snake
+y = 60
+sn_size = 20
+clock = pygame.time.Clock()  # it handles the clock of game
+fps = 60
+in_velocity = 5
+speed_x = 5
+speed_y = 0  ##we give speed or velocity to our snake
 ##lets make score print on display board
-font=pygame.font.SysFont(None,20)
-def text_screen(text,color,x,y):
-    screen=font.render(text,True,color)##this take 3 variables and print in
-    gameWindow.blit(screen,(x,y))##blit func prints in on screen
-    
+font = pygame.font.SysFont(None, 20)
+
+##for increasing length of snake
+snk_list=[]##we make a empty list and increase it but we make it list of list using head list
+snk_length=1
+
+def text_screen(text, color, x, y):
+    screen = font.render(text, True, color)  ##this take 3 variables and print in window
+    gameWindow.blit(screen, (x, y))  ##blit func prints in on screen
+#the snake function makes the length of the snake
+def plot_snake(gameWindow, color, snk_list, sn_size):
+    # print(snk_list)
+    ##we display every element of the snk_list and append accordingly
+    for a,b in snk_list:
+        pygame.draw.rect(gameWindow, red, [a, b, sn_size, sn_size])
+
+
 ##lets make food for our snake random function returns random variable between a nad b
-food_x=random.randint(10,width/2)
-food_y=random.randint(10,height/2)
-score=0##for making score go up
+food_x = random.randint(10, width / 2)
+food_y = random.randint(10, height / 2)
+score = 0  ##for making score go up
 
 ##Colors
-white=(255,255,255)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-black=(0,0,0)
-
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
 
 ##Game Loop
 while not exit_game:
-    for event in pygame.event.get(): 
+    for event in pygame.event.get():
         ##print(event)
-        if event.type==pygame.QUIT:
-            exit_game=True
+        if event.type == pygame.QUIT:
+            exit_game = True
 
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_RIGHT:
-                speed_x=in_velocity
-                speed_y=0
-       
-            if event.key==pygame.K_LEFT:
-                speed_x=-in_velocity
-                speed_y=0
-       
-            if event.key==pygame.K_UP:
-                speed_y=-in_velocity
-                speed_x=0
-        
-            if event.key==pygame.K_DOWN:
-                speed_y=in_velocity
-                speed_x=0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                speed_x = in_velocity
+                speed_y = 0
 
-    #changing speed
-    x+=speed_x
-    y+=speed_y
-    
-##    if x>=height:
-##        x=0
-##    if y>=width:
-##        y=0
-##        x=0
-    #window color
+            if event.key == pygame.K_LEFT:
+                speed_x = -in_velocity
+                speed_y = 0
+
+            if event.key == pygame.K_UP:
+                speed_y = -in_velocity
+                speed_x = 0
+
+            if event.key == pygame.K_DOWN:
+                speed_y = in_velocity
+                speed_x = 0
+
+    # changing speed
+    x += speed_x
+    y += speed_y
+
+    ##    if x>=height:
+    ##        x=0
+    ##    if y>=width:
+    ##        y=0
+    ##        x=0
+    # window color
     gameWindow.fill(white)
-    text_screen("Score:"+str(score),blue,15,15)
+    text_screen("Score:" + str(score), blue, 15, 15)
     ##prints score on display
-     
+
     ##Making head of snake
-    pygame.draw.rect(gameWindow, red, [x,y,sn_size,sn_size])
+    # pygame.draw.rect(gameWindow, red, [x, y, sn_size, sn_size])
+    ##now we make heads of head by a func
+    plot_snake(gameWindow,red, snk_list,sn_size)##this plots the coordinates to areas
+    ##snk_list contains the coordinates of the positions
+
 
     ##making food and eating it
-    pygame.draw.rect(gameWindow, green, [food_x,food_y,sn_size,sn_size])
-    if abs(x-food_x)<15 and abs(y-food_y)<15:
-        score+=10
+    pygame.draw.rect(gameWindow, green, [food_x, food_y, sn_size, sn_size])
+    if abs(x - food_x) < 15 and abs(y - food_y) < 15:
+        score += 10
         ##print("Score:", score)
-        food_x=random.randint(10,width/1.5)
-        food_y=random.randint(10,height/2)
+        food_x = random.randint(10, width / 1.5)
+        food_y = random.randint(10, height / 2)
+        snk_length+=5#only when the snake eats food its size should be increased
+
+    ##we append head because initially we need a head
+    head=[]
+    head.append(x)##it stores the previous value of head bcz x and y changes in every loop
+    head.append(y)
+    snk_list.append(head)
+    if len(snk_list)>snk_length:
+        del snk_list[0]##only when the snake eats food its size should be increased
+        ##whenever size of snake is more than len we del first element. Snk_len controls the size of snk,list
+    
     ##how many frames in one sec
     clock.tick(fps)
-        #after every change update
+    # after every change update
     pygame.display.update()
-    
+
 pygame.quit()
 quit()
