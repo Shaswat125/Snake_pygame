56af23f13e249a0bfbc7ad4ed3635fa814578e18
SHASWAT <shaswatanand125@gmail.com>
2021-06-02 15:54:47 +0530

    High score is saved in a text file

--- Snake_game.py
+++ Snake_game.py
@@ -1,15 +1,18 @@
 import pygame
 import random
 
+
+
 pygame.init()
 width = 900
 height = 600
 ##Game window
 gameWindow = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Mick's Snake game")
+pygame.display.set_caption("Vintage Snake game")
 pygame.display.update()
 
 font = pygame.font.SysFont(None, 40)
+
 def text_screen(text, color, x, y):
     screen = font.render(text, True, color)  ##this take 3 variables and print in window
     gameWindow.blit(screen, (x, y))  ##blit func prints in on screen
@@ -20,9 +23,32 @@ def plot_snake(gameWindow, color, snk_list, sn_size):
     for a,b in snk_list:
         pygame.draw.rect(gameWindow, red, [a, b, sn_size, sn_size])
     # print(snk_list)
-    ##we display every element of the snk_list and append accordingly
+    ## display every element of the snk_list and append accordingly
 
+#Welcome screen of the game
+def welcome():
+    fps=60
+    clock = pygame.time.Clock()
+    exit_game=False
+    while not exit_game:
+        gameWindow.fill((45,67,88))
+        text_screen("Welcome to Snake game, Press Enter", black,180,245)
+        text_screen("Press Esc to end", white, 220, 305)
+        for event in pygame.event.get():
+            ##print(event)
+            if event.type == pygame.QUIT:
+                exit_game = True
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                  gameloop()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+            pygame.display.update()
+            clock.tick(fps)
+            # after every change update
 
 
 ##Colors
@@ -31,6 +57,8 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 black = (0, 0, 0)
+
+
 
 ##Game Loop
 def gameloop():
@@ -47,19 +75,29 @@ def gameloop():
     speed_y = 0  ##we give speed or velocity to our snake
     ##lets make score print on display board
 
+    ##Now we put high score to our program
+    f = open("HighScore.txt", "r")
+    hi_score = f.read()
+    f.close()
+    ##another way to store high
+    # with open("Highscore.txt","r") as f:
+    #     hi_score=f.read()
 
     ##for increasing length of snake
     snk_list = []  ##we make a empty list and increase it but we make it list of list using head list
     snk_length = 1
 
     ##lets make food for our snake random function returns random variable between a nad b
-    food_x = random.randint(10, width / 2)
-    food_y = random.randint(10, height / 2)
+    food_x = random.randint(30, width / 2)
+    food_y = random.randint(30, height / 2)
     score = 0  ##for making score go up
 
 
     while not exit_game:
         if game_over:
+            f = open("HighScore.txt", "w")
+            f.write(str(hi_score))
+            f.close()
             gameWindow.fill(white)
             text_screen("Game Over! Press enter to play again.", black, 170, 240)
             for event in pygame.event.get():
@@ -68,7 +106,7 @@ def gameloop():
                     exit_game = True
                 if event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_RETURN:
-                        gameloop()
+                        welcome()
         #this was to make game over when snake hits walls and ask to restart
 
         else:
@@ -87,12 +125,20 @@ def gameloop():
                         speed_y = 0
 
                     if event.key == pygame.K_UP:
-                        speed_y = -in_velocity
                         speed_x = 0
+                        speed_y = -in_velocity
+                       
 
                     if event.key == pygame.K_DOWN:
-                        speed_y = in_velocity
                         speed_x = 0
+                        speed_y = in_velocity
+                        
+                    #For cheat codes, to increase score without eating food is c
+                    if event.key == pygame.K_c:
+                        score+=10
+                    #cheat code to decrease velocity is v
+                    if event.key == pygame.K_v:
+                        in_velocity-=1
 
             # changing speed
             x += speed_x
@@ -103,7 +149,7 @@ def gameloop():
                 # print("Game over")
             # window color
             gameWindow.fill(white)
-            text_screen("Score:" + str(score), blue, 15, 15)
+            text_screen("Score:" + str(score)+ "  High Score:"+str(hi_score), blue, 15, 15)
             ##prints score on display
 
             ##Making head of snake
@@ -122,6 +168,12 @@ def gameloop():
                 food_y = random.randint(10, height / 2)
                 snk_length+=5
                 #only when the snake eats food its size should be increased
+                if score>int(hi_score):
+                    hi_score=score
+                    f = open("HighScore.txt", "w")
+                    f.write(str(hi_score))
+                    f.close()
+
 
             #we append head because initially we need a head
             head=[]
@@ -149,5 +201,8 @@ def gameloop():
 
     pygame.quit()
     quit()
+
+welcome()
 gameloop()
 #We make the whole game loop and its variables inside a game loop funct and call that fun when u press enter
+
